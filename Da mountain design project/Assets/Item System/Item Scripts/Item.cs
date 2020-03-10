7af1020dde22db512191c544ea82f0cfa590707d
Alexcool3 <36537085+Alexcool3@users.noreply.github.com>
2020-03-10 15:21:00 +0100

    Item System Updated
    - Item pick up added
    - Game Events manager and GameManager added.

--- Da mountain design project/Assets/Item System/Item Scripts/Item.cs
+++ Da mountain design project/Assets/Item System/Item Scripts/Item.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [HideInInspector] public int itemID;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        GameEvents.gameEvents.ItemPickUpTriggerEnter(itemID);
     }
 }
---
+++ Da mountain design project/Assets/Singleton Managers/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region GameManager Ínstance
+    public static GameManager gameManager
+    {
+
+        get
+        {
+            return instance;
+        }
+
+        private set
+        {
+            if (instance != null)
+            {
+                Destroy(value.gameObject);
+                return;
+            }
+
+            instance = value;
+
+        }
+    }
+    private static GameManager instance = null; // Singleton Instance
+    #endregion
+
+    [SerializeField] private Transform[] spawnPoints = null;
+
+    private void Awake()
+    {
+        gameManager = this;
+        DontDestroyOnLoad(gameObject);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            spawnPoints[i].GetComponent<ItemController>().itemID = i;
+        }
+    }
+
+}
