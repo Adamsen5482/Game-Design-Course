e516ad5030d01c1dda1b79d529edec31d1bf7e64
Morten Bach <36537080+mbja17@users.noreply.github.com>
2020-04-14 15:39:16 +0200

    WinCinematic

---
+++ Da mountain design project/Assets/fadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeIn : MonoBehaviour
+{
+    private float t;
+    public GameObject winCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (winCanvas.activeSelf){
+            if(GetComponent<CanvasGroup>().alpha < 1){
+                t += 0.5f * Time.deltaTime;
+                GetComponent<CanvasGroup>().alpha = Mathf.Lerp(0f, 1f, t);
+            }
+        }
+    }
+}
