b5654b60d70cc91e11d204560d5de3c4b1af680c
Morten Bach <36537080+mbja17@users.noreply.github.com>
2020-04-14 15:47:20 +0200

    WinState

--- Da mountain design project/Assets/win.cs
+++ Da mountain design project/Assets/win.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class win : MonoBehaviour
 {
     public UnityEngine.GameObject winCanvas;
@@ -25,7 +26,10 @@ public class win : MonoBehaviour
             string min = ((int)timer.currentTime / 60).ToString();
             string sec = (timer.currentTime % 60).ToString("f0");
             winTime.text = "YOUR TIME: \n" + min + " minutes and " + sec + " seconds";
-            winCanvas.SetActive(true);
+            //winCanvas.SetActive(true);
+            //new line
+            SceneManager.LoadScene("WinCinematic");
+
             other.gameObject.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
