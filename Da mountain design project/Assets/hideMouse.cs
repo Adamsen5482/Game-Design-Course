c79216c7d8f165286a263814dc5d5bb331c65f42
Morten Bach <36537080+mbja17@users.noreply.github.com>
2020-04-17 21:30:10 +0200

    hide mouse when not in menu

---
+++ Da mountain design project/Assets/hideMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideMouse : MonoBehaviour
+{
+    public GameObject winCanvas;
+    public GameObject loseCanvas;
+    public GameObject leaderboardCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(winCanvas.active || loseCanvas.active || leaderboardCanvas.active){
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        print("mouseTrue");
+        }
+        else{
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
